2301445c7bda67eead3fedfbb3d908a2139a44a7
Aman kumar <a.k.bullet00@gmail.com>
2024-08-18 13:44:50 +0530

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,20 +8,28 @@ const Footer = () => {
                     <div className="container py-2">
                         <div className="row footer2">
                             <div className="col-md-4 footercol1">
-                                <img src="https://gomainspring.com/wp-content/uploads/2023/12/mainspring-logo-cybersecurity-it-services-automation.png" alt="MainSpring Logo" width='200px'/>
+                                <img src="https://gomainspring.com/wp-content/uploads/2023/12/mainspring-logo-cybersecurity-it-services-automation.png" alt="MainSpring Logo" width='200px' />
                                 <p>MainSpring, Inc.</p>
                             </div>
                             <div className="col-md-4">
                                 <p><i className="fas fa-map-marker-alt" /> 8 E. Second Street, Suite 205, Frederick, MD 21701</p>
                                 <p><i className="fas fa-phone" /> 301.948.8077</p>
                             </div>
-                            <div className="col-md-4 text-right" style={{width:'300px'}}>
+                            <div className="col-md-4 text-right" style={{ width: '300px' }}>
                                 <p><i className="fas fa-envelope" /> hello@gomainspring.com</p>
                                 <div className="social-icons">
-                                    <a href="#"><i className="fab fa-facebook-f" /></a>
-                                    <a href="#"><i className="fab fa-linkedin-in" /></a>
-                                    <a href="#"><i className="fab fa-xing" /></a>
-                                    <a href="#"><i className="fab fa-youtube" /></a>
+                                    <a href="#">
+                                        <i className="fab fa-facebook-f navIcon" />
+                                    </a>
+                                    <a href="#">
+                                        <i className="fab fa-linkedin-in navIcon" />
+                                    </a>
+                                    <a href="#">
+                                        <i className="fab fa-xing navIcon" />
+                                    </a>
+                                    <a href="#">
+                                        <i className="fab fa-youtube navIcon" />
+                                    </a>
                                 </div>
                             </div>
                         </div>
